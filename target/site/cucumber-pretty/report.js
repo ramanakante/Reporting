d8f7d42aa554d53587c9a0801030d090a2c7cecf
ramanakante <ramana.kante@gmail.com>
2018-01-07 06:27:49 -0500

    Initiated files

---
+++ target/site/cucumber-pretty/report.js
@@ -0,0 +1,170 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "LoginFeature",
+  "description": "This feature deals with the login functionality of the application",
+  "id": "loginfeature",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 2745852396,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Login with correct username and password",
+  "description": "",
+  "id": "loginfeature;login-with-correct-username-and-password",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I navigate to the login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter the following for Login",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 7
+    },
+    {
+      "cells": [
+        "admin",
+        "adminpassword"
+      ],
+      "line": 8
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I click login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I should see the userform page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStep.iNavigateToTheLoginPage()"
+});
+formatter.result({
+  "duration": 2878463250,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.iEnterTheFollowingForLogin(DataTable)"
+});
+formatter.result({
+  "duration": 219384452,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.iClickLoginButton()"
+});
+formatter.result({
+  "duration": 189468331,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.iShouldSeeTheUserformPage()"
+});
+formatter.result({
+  "duration": 37311971,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 146686,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1888394236,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "Login with correct username and password to fail",
+  "description": "",
+  "id": "loginfeature;login-with-correct-username-and-password-to-fail",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 13,
+  "name": "I navigate to the login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 14,
+  "name": "I enter the following for Login",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 15
+    },
+    {
+      "cells": [
+        "admin",
+        "adminpassword"
+      ],
+      "line": 16
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I should see the userform page wrongly",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStep.iNavigateToTheLoginPage()"
+});
+formatter.result({
+  "duration": 2487734878,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.iEnterTheFollowingForLogin(DataTable)"
+});
+formatter.result({
+  "duration": 169339505,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.iClickLoginButton()"
+});
+formatter.result({
+  "duration": 169158180,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStep.iShouldSeeTheUserformPageWrongly()"
+});
+formatter.result({
+  "duration": 35233999,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"id\",\"selector\":\"sdfgdsfsd\"}\n  (Session info: chrome\u003d63.0.3239.108)\n  (Driver info: chromedriver\u003d2.34.522940 (1a76f96f66e3ca7b8e57d503b4dd3bccfba87af1),platform\u003dWindows NT 10.0.16299 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 30 milliseconds\nFor documentation on this error, please visit: http://seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.0.1\u0027, revision: \u00271969d75\u0027, time: \u00272016-10-18 09:49:13 -0700\u0027\nSystem info: host: \u0027DESKTOP-FANSEVQ\u0027, ip: \u0027192.168.33.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_152\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities [{applicationCacheEnabled\u003dfalse, rotatable\u003dfalse, mobileEmulationEnabled\u003dfalse, networkConnectionEnabled\u003dfalse, chrome\u003d{chromedriverVersion\u003d2.34.522940 (1a76f96f66e3ca7b8e57d503b4dd3bccfba87af1), userDataDir\u003dC:\\WINDOWS\\TEMP\\scoped_dir9208_32404}, takesHeapSnapshot\u003dtrue, pageLoadStrategy\u003dnormal, databaseEnabled\u003dfalse, handlesAlerts\u003dtrue, hasTouchScreen\u003dfalse, version\u003d63.0.3239.108, platform\u003dXP, browserConnectionEnabled\u003dfalse, nativeEvents\u003dtrue, acceptSslCerts\u003dtrue, locationContextEnabled\u003dtrue, webStorageEnabled\u003dtrue, browserName\u003dchrome, takesScreenshot\u003dtrue, javascriptEnabled\u003dtrue, cssSelectorsEnabled\u003dtrue, setWindowRect\u003dtrue, unexpectedAlertBehaviour\u003d}]\nSession ID: 4dcf5245beeafc147af8bfd51342afdc\n*** Element info: {Using\u003did, value\u003dsdfgdsfsd}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:216)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:168)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:635)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:368)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementById(RemoteWebDriver.java:417)\r\n\tat org.openqa.selenium.By$ById.findElement(By.java:218)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:360)\r\n\tat steps.LoginStep.iShouldSeeTheUserformPageWrongly(LoginStep.java:71)\r\n\tat ✽.Then I should see the userform page wrongly(Login.feature:18)\r\n",
+  "status": "failed"
+});
+formatter.after({
+  "duration": 79972,
+  "status": "passed"
+});
+});
